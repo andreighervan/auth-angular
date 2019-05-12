02262d1c8d31c43923aaf1e701f04289d45a903a
andreighervan <andrei.ghervan8@gmail.com>
2019-05-12 17:05:28 +0300

    update server.js

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const cors=require('cors');
 
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
 
 const api=require('./routes/api')
 const app=express()
 app.use(cors());
+app.use(express.static(__dirname + '/ngApp/dist'));
 
 app.use(bodyParser.json())
 
@@ -16,6 +17,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname+'/ngApp/dist/index.html'))
 })
 
-app.listen(PORT, ()=>{
-    console.log('Server running on localhst '+PORT)
-})
+ app.listen(PORT, ()=>{
+     console.log('Server running on localhst '+PORT)
+ })
